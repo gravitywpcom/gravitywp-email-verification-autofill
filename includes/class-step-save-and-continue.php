83dc1bba5c002aa6afedf52c35bb307d53843649
Yoren Chang <yorenchang@gmail.com>
2017-07-10 19:32:27 +0800

    Trivial edits: update tooltip text

--- includes/class-step-save-and-continue.php
+++ includes/class-step-save-and-continue.php
@@ -53,7 +53,7 @@ if ( class_exists( 'Gravity_Flow_Step' ) ) {
 						'label'             => esc_html__( 'Date Created', 'gravitywpsaveandcontinue' ),
 						'required'          => false,
 						'class'             => 'small',
-						'tooltip'           => esc_html__( 'Specify the date_created fields in the rg_incomplete_submissions table. It needs to be in the ISO date format: YYYY-mm-dd HH:ii:ss (eg. 2017-07-31 23:59:59). <br /><br /> Leave empty if you don\'t need to force the date.', 'gravitywpsaveandcontinue' ),
+						'tooltip'           => esc_html__( 'Specify the date_created fields in the rg_incomplete_submissions table. It needs to be in the ISO date format: <strong>YYYY-mm-dd HH:ii:ss (eg. 2017-07-31 23:59:59)</strong>. <br /><br /> Leave empty if you don\'t need to force the date.', 'gravitywpsaveandcontinue' ),
 						'feedback_callback' => array( $this, 'validate_date_created' ),
 					),
 					array(
