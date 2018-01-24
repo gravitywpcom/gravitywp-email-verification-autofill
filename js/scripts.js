4f821b41de848affbbbf534d6e1a6b1db95029b2
Yoren Chang <yorenchang@gmail.com>
2018-01-24 13:21:07 +0800

    Create the scripts to autofill the email verification field

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-jQuery(document).ready(function($){
-    
+jQuery(document).ready(function ($) {
+    if ($('.ginput_container_email').length > 0) {
+        var email = $('.ginput_container_email input:first'),
+            emailId = email.attr('id'),
+            emailVal = email.val();
+
+        if (emailVal !== '') {
+            $('#' + emailId + '_2').val(emailVal);
+        }
+    }
 });
