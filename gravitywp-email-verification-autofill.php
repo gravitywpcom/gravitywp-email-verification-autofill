7abe931e7b5f61d63a450d76997a72d1451e987a
Yoren Chang <yorenchang@gmail.com>
2018-01-24 12:39:32 +0800

    Initial commit

---
+++ gravitywp-email-verification-autofill.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name:       	GravityWP Email Verification Autofill
+ * Plugin URI:        	http://gravitywp.com/
+ * Description:       	Autofill email verification field when loading the form on the front end (editing entry with Gravity View shortcode).
+ * Version:          	1.0.0
+ * Author:            	GravityWP
+ * Author URI:        	http://gravitywp.com/
+ * License:           	GPLv2 or later
+ * License URI: 		http://www.gnu.org/licenses/gpl-2.0.html
+ */
---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== GravityWP Email Verification Autofill ===
+Contributors: erikvanbeek, yoren
+Tags: gravity forms, gravity view
+Requires at least: 4.0
+Tested up to: 4.9.2
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Autofill email verification field when loading the form on the front end (editing entry with Gravity View shortcode).
+
+@todo More Readme content to go.
